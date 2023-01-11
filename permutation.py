c0ac49137698d96928b64d57a5e4d2626aa046f0
Shams <49056370+ShamsArfeen@users.noreply.github.com>
2023-01-11 23:40:48 +0500

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,18 +1,18 @@
-def permute(n, s, a):
+def permute(m, s, a):
     ''' 
     Returns YES if you can append several elements to the array
     a, that their sum equals S and the resultant array is a permutation. Returns NO 
     otherwise.
 
     Parameters:
-    - n : Lenght of array a
+    - m : Lenght of array a
     - s : Sum of missing numbers of permutation
     - a : Known numbers of permutation
     
     Constraints:
-    - 1 <= n <= 100
+    - 1 <= m <= 100
     - 1 <= s <= 10000
-    - 1 <= a_i <= 1000
+    - 1 <= a_i <= 100
     '''
     s += sum(a)
     sm = 0
@@ -28,8 +28,8 @@ def permute(n, s, a):
 T = int(input())
 f = []
 for i in range(T):
-    n,s = [int(i) for i in input().split()]   
+    m,s = [int(i) for i in input().split()]   
     lst = [int(i) for i in input().split()]
-    ans = permute(n,s,lst)
+    ans = permute(m,s,lst)
     print(ans)   
     f.append(ans)
