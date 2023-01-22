8eaf0d6b3e776f719e5830e69820ed76d911505c
Aquib Ansari <77619536+aqib420@users.noreply.github.com>
2023-01-22 21:00:52 +0500

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -1,3 +1,5 @@
+# import ipdb; 
+# ipdb.set_trace()
 def permute(m, s, a):
     ''' 
     Returns YES if you can append several elements to the array
@@ -15,3 +17,35 @@ def permute(m, s, a):
     - 1 <= a_i <= 100
     '''
     pass
+    # m = int(input())
+    # s= int(input())
+    # a= int(input())
+    
+
+    lst=[]
+    for i in range(1,100):
+        lst.append(i)
+    for j in a:
+        if j in lst:
+            lst.remove(j)
+        
+    
+    for i in range(len(lst)):
+        if s>0:
+            s=s-lst[i]
+            a.append(lst[i])
+        
+    # for i in range(len(lst)):
+    #     while s>0:
+    #         s=s-lst[i]
+    #         a.append(lst[i])
+    if s!=0:
+        return("NO")
+    else:
+        return ("YES")
+
+
+
+
+
+# print(permute(m,s,a))
